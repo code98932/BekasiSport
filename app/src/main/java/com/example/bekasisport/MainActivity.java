06a7395d7889ff22bc169c46d5cb402f19d16141
nazzalaihsan <nazzalaihsan217@gmail.com>
2021-06-21 16:46:57 +0700

    Button b

--- app/src/main/java/com/example/bekasisport/MainActivity.java
+++ app/src/main/java/com/example/bekasisport/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.bekasisport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonNewsPage;
+    private Button buttonCommunityPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonNewsPage = (Button) findViewById(R.id.homeNews);
+        buttonNewsPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewsHomePages();
+            }
+
+
+        });
+
+        buttonCommunityPage =(Button) findViewById(R.id.homeCommunity);
+        buttonCommunityPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCommunityPages();
+            }
+
+
+        });
+
+    }
+
+    private void openNewsHomePages() {
+        Intent intent = new Intent(this, NewsActivity.class);
+        startActivity(intent);
+    }
+
+    private void openCommunityPages() {
+
+        Intent intent = new Intent(this, CommunityActivity.class);
+        startActivity(intent);
+
     }
 }
