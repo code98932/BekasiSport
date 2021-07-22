28ef749613bfde07a5e6c34d435344bea4f5d19f
nazzalaihsan <nazzalaihsan217@gmail.com>
2021-07-22 09:14:04 +0700

    :P

--- app/src/main/java/com/example/bekasisport/Comunity/Football/FootballData.java
+++ app/src/main/java/com/example/bekasisport/Comunity/Football/FootballData.java
@@ -8,22 +8,45 @@ public class FootballData {
 
 
     private static final String[] footballName = {
-            "Persikasi",
             "Persipasi Bekasi",
-            ""
+            "MBSB",
+            "ASPEK FC",
+            "Sakisega",
+            "ABA FC"
+
+
+
+
 
     };
 
     private static final String[] footballDetail = {
             "Persikasi adalah singkatan dari Persatuan Sepak Bola Indonesia Kabupaten Bekasi . Persikasi Bekasi adalah klub sepak bola Indonesia yang berbasis di Kabupaten Bekasi, Jawa Barat.",
             "KALAH MENANG SUDAH BIASA PERSAHABATAN LUAR BIASA",
+            "Jaga sportifitas dan menjalin silaturahmi",
+
+            "official sakisega\n" +
+                    "open sparing\n" +
+                    "silaturahmi bareng\n" +
+                    "football dan futsal\n",
+
+            "Football for friendship\n" +
+                    "Kp.Cibitung Babakan Kelurahan Telaga Asih Kecamatan Cikarang Barat Kabupaten Bekasi"
+
+
 
 
 
     };
 
     private static final int[] footballImage= {
-            R.drawable.k
+            R.drawable.k,
+            R.drawable.zacky,
+            R.drawable.dede,
+            R.drawable.wq,
+            R.drawable.we
+
+
 
     };
 
--- app/src/main/java/com/example/bekasisport/Comunity/Swimming/SwimmingData.java
+++ app/src/main/java/com/example/bekasisport/Comunity/Swimming/SwimmingData.java
@@ -8,15 +8,23 @@ import java.util.Collection;
 public class SwimmingData {
 
     private static final String[] swimName = {
-            "test"
+            "Kolam renang Sarang Walet",
+            "Kolam Renang Indoor",
+            "Go Splash Panjibuwono"
+
     };
 
     private static final String[] swimDetail = {
-            "keong"
+            "Kolam renang sarang walet adalah tempat rekreasi untuk keluarga dengan suasana yang nyaman, bersih, dan murah.",
+            "Indoor waterpark pertama di Indonesia dibangun tahun 2013 adalah tempat rekreasi yang sangat cocok untuk anak-anak. Didesain dengan konsep ala pertunjukan sirkus, Waterboom ini juga melindungi anak-anak dari kepanasan dan kehujanan, ada kolam air panas, lantai dari rubber yang safety dan nyaman. Ada fasilitas sauna dan Mom n Baby Spa membuat Sirkus Waterplay menjadi tempat favorite bagi keluarga",
+
+
     };
 
     private static final int[] swimImage= {
-            R.drawable.test
+            R.drawable.zx,
+            R.drawable.vc,
+            R.drawable.cb
 
     };
 
